bac6552013740e2292a79cc691556e26ac3180e4
Edward-Odey <edwardodey04@gmail.com>
2024-04-05 15:09:14 +0100

    update 0-validate_utf8.py pycodestyle

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,36 +3,42 @@
 
 
 def validUTF8(data):
-    """determines if a given data set represents a valid UTF-8 encoding"""
+    """method that determines if a given data set
+    represents a valid UTF-8 encoding."""
 
-    # Variable to keep track of the number of remaining bytes
-    # to read for a character
-    remaining_byte = 0
+    # Variable to keep track of the number of remaining
+    # bytes to read for a character
+    remaining_bytes = 0
 
-    for value in data:
-
+    # Iterate through each byte in the data
+    for byte in data:
         # Check if this byte is a continuation byte
-        if remaining_byte:
-
+        if remaining_bytes:
             # If it's not of the form 10xxxxxx, then it's an
             # invalid continuation byte
-            if value >> 6 == 0b10:
-                remaining_byte -= 1
-                continue
-            return False
-
-        # If the byte starts with 0, it's a single-byte character
-        if value >> 7 == 0:
-            continue
-        else:
-            # Count the number of leading 1s to determine the number
-            # of bytes in this character
-            if value >> 3 == 0b11110:
-                remaining_byte = 3
-            elif value >> 4 == 0b1110:
-                remaining_byte = 2
-            elif value >> 5 == 0b110:
-                remaining_byte = 1
-            else:
+            if byte >> 6 != 0b10:
                 return False
+            remaining_bytes -= 1
+        else:
+            # If the byte starts with 0, it's a single-byte character
+            if byte >> 7 == 0:
+                continue
+            # Count the number of leading 1s to determine the
+            # number of bytes in this character
+            elif byte >> 3 == 0b11110:
+                remaining_bytes = 3
+            elif byte >> 4 == 0b1110:
+                remaining_bytes = 2
+            elif byte >> 5 == 0b110:
+                remaining_bytes = 1
+            else:
+                # Invalid starting byte
+                return False
+
+    # If there are remaining bytes to be read after iterating through
+    # all data, it's invalid
+    if remaining_bytes:
+        return False
+
+    # If everything passes, it's a valid UTF-8 encoding
     return True
