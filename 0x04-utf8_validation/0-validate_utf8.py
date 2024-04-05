00e03f754ab627f5c604e0d7e28d02e5aedd4ca1
Edward-Odey <edwardodey04@gmail.com>
2024-04-05 15:28:31 +0100

    update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,40 +5,26 @@
 def validUTF8(data):
     """method that determines if a given data set
     represents a valid UTF-8 encoding."""
-
-    # Variable to keep track of the number of remaining
-    # bytes to read for a character
     remaining_bytes = 0
 
-    # Iterate through each byte in the data
     for byte in data:
-        # Check if this byte is a continuation byte
+        byte_value = byte & 0xFF
+
         if remaining_bytes:
-            # If it's not of the form 10xxxxxx, then it's an
-            # invalid continuation byte
-            if byte >> 6 != 0b10:
+            if byte_value >> 6 != 0b10:
                 return False
             remaining_bytes -= 1
         else:
-            # If the byte starts with 0, it's a single-byte character
-            if byte >> 7 == 0:
-                continue
-            # Count the number of leading 1s to determine the
-            # number of bytes in this character
-            elif byte >> 3 == 0b11110:
+            if byte_value >> 3 == 0b11110:
                 remaining_bytes = 3
-            elif byte >> 4 == 0b1110:
+            elif byte_value >> 4 == 0b1110:
                 remaining_bytes = 2
-            elif byte >> 5 == 0b110:
+            elif byte_value >> 5 == 0b110:
                 remaining_bytes = 1
-            else:
-                # Invalid starting byte
+            elif byte_value >> 7 != 0:
                 return False
 
-    # If there are remaining bytes to be read after iterating through
-    # all data, it's invalid
     if remaining_bytes:
         return False
 
-    # If everything passes, it's a valid UTF-8 encoding
     return True
