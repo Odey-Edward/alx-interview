902fbe2dc921bc30440f529ce40b247335058826
Edward-Odey <edwardodey04@gmail.com>
2024-05-06 22:17:57 +0100

    0-making_change.py: implented make change with dynamic programming

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
+"""make change module"""
+
 
 def makeChange(coins, total):
+    """Make change method"""
     min_ch = [float('inf')] * (total + 1)
-    min_ch[0] = 0;
+    min_ch[0] = 0
 
     if total <= 0:
-        return 0;
+        return 0
 
     for v in range(1, total + 1):
         for coin in coins:
             if coin <= v:
                 min_ch[v] = min((min_ch[v - coin] + 1), min_ch[v])
 
-
     if min_ch[total] != float('inf'):
         return min_ch[total]
     else:
         return -1
-
-
-print(makeChange([1,2,5], 11))
