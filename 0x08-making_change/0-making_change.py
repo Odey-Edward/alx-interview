e0fa9747125bf6ea1ca1903bb1749763dc2f387d
Edward-Odey <edwardodey04@gmail.com>
2024-05-06 22:27:18 +0100

    0-making_change.py: update logic

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,10 +5,11 @@
 def makeChange(coins, total):
     """Make change method"""
     min_ch = [float('inf')] * (total + 1)
-    min_ch[0] = 0
 
     if total <= 0:
         return 0
+
+    min_ch[0] = 0
 
     for v in range(1, total + 1):
         for coin in coins:
